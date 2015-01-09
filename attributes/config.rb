ae5a7d3526e0f2863d557b43001303074da5a9e5
David Bajot <dbajot@cask.co>
2015-01-08 16:32:56 -0800

    add known impala start up configurations to /etc/default/impala

--- attributes/config.rb
+++ attributes/config.rb
@@ -27,3 +27,21 @@ default['impala']['group'] = 'impala'
 # Set Hadoop attributes
 override['hadoop']['distribution'] = 'cdh'
 override['hadoop']['distribution_version'] = 5
+
+# Impala /etc/default/impala startup configurations
+default['impala']['config']['IMPALA_CATALOG_SERVICE_HOST'] = '127.0.0.1'
+default['impala']['config']['IMPALA_STATE_STORE_HOST'] = '127.0.0.1'
+default['impala']['config']['IMPALA_STATE_STORE_PORT'] = '24000'
+default['impala']['config']['IMPALA_BACKEND_PORT'] = '22000'
+default['impala']['config']['IMPALA_LOG_DIR'] = '/var/log/impala'
+default['impala']['config']['IMPALA_CATALOG_ARGS'] = ' -log_dir=${IMPALA_LOG_DIR} '
+default['impala']['config']['IMPALA_STATE_STORE_ARGS'] = ' -log_dir=${IMPALA_LOG_DIR} -state_store_port=${IMPALA_STATE_STORE_PORT}'
+default['impala']['config']['IMPALA_SERVER_ARGS'] = ' \
+    -log_dir=${IMPALA_LOG_DIR} \
+    -catalog_service_host=${IMPALA_CATALOG_SERVICE_HOST} \
+    -state_store_port=${IMPALA_STATE_STORE_PORT} \
+    -use_statestore \
+    -state_store_host=${IMPALA_STATE_STORE_HOST} \
+    -be_port=${IMPALA_BACKEND_PORT}'
+default['impala']['config']['ENABLE_CORE_DUMPS'] = 'false'
+
