07af1bc777b541c6837dba2bb1da402d31a0548c
Chris Gianelloni <chris@cask.co>
2015-01-14 18:31:08 -0800

    Version bump to 0.1.0

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name             'impala'
-maintainer       'Cask'
+maintainer       'Cask Data, Inc.'
 maintainer_email 'ops@cask.co'
 license          'All rights reserved'
 description      'Installs/Configures Impala'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.1'
+version          '0.1.0'
 
 %w(hadoop java).each do |cb|
   depends cb
